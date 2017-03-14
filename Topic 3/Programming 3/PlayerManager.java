d79cea8b51dbb4ed57517b589ef61554ac1134a2
rsqtl94 <rose_sqtl@hotmail.com>
2017-03-13 20:17:48 -0700

    Update PlayerManager.java

--- Topic 3/Programming 3/PlayerManager.java
+++ Topic 3/Programming 3/PlayerManager.java
@@ -1,7 +1,32 @@
 
 public class PlayerManager {
 	public static void main(String[] args) {
-			
-}
+	NFLPlayer[] NFLPlayersArray = new NFLPlayer[6];
 
+	NFLPlayersArray[0] = new NFLPlayer();
+	NFLPlayersArray[0].firstName = "Tom";
+	NFLPlayersArray[0].lastName = "Santos";
+	
+	NFLPlayersArray[1] = new NFLPlayer();
+	NFLPlayersArray[1].firstName = "Matt";
+	NFLPlayersArray[1].lastName = "Rodger";
+	
+	NFLPlayersArray[2] = new NFLPlayer();
+	NFLPlayersArray[2].firstName = "Aaron";
+	NFLPlayersArray[2].lastName = "Stamos";
+	
+	NFLPlayersArray[3] = new NFLPlayer();
+	NFLPlayersArray[3].firstName = "Ben";
+	NFLPlayersArray[3].lastName = "Jauregui";
+	
+	NFLPlayersArray[4] = new NFLPlayer();
+	NFLPlayersArray[4].firstName = "Russell";
+	NFLPlayersArray[4].lastName = "Brown";
+	
+	NFLPlayersArray[5] = new NFLPlayer();
+	NFLPlayersArray[5].firstName = "Jesse";
+	NFLPlayersArray[5].lastName = "Levine";
+	
+	
+	}
 }
