316c120dae0112d8c2de90e00ec419943f2daa9d
rsqtl94 <rose_sqtl@hotmail.com>
2017-03-22 15:25:07 -0700

    Update createGettersAndSetters.java

--- Topic 4/createGettersAndSetters.java
+++ Topic 4/createGettersAndSetters.java
@@ -1,4 +1,35 @@
 
-public class createGettersAndSetters {
 
-}
+public class createGettersAndSetters<string> {
+
+	    private string _firstName;
+	    private string _lastName;
+	    private string _personDescription;
+	    private string _other;
+
+	    public createGettersAndSetters() { }
+
+	    //same for lastName, personDescription, other
+	    public string FirstName
+	    {
+	        get
+	        {
+	            return _firstName;
+	        }
+	        set
+	        {
+	            _firstName = FixValue(value);
+	        }
+	    }
+
+	    private string FixValue(string value)
+	    {
+	        value = value.Trim();
+	        if (value == string.Empty)
+	        {
+	            return null;
+	        }
+
+	        return value;
+	    }
+	}
