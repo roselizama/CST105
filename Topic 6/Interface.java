83c4fb346735e604ce55b5eb61301e692bdadb9b
rsqtl94 <rose_sqtl@hotmail.com>
2017-04-05 16:12:20 -0700

    Update Interface.java

--- Topic 6/Interface.java
+++ Topic 6/Interface.java
@@ -1,5 +1,32 @@
-package work;
-
-public class Inheritance {
-
-}
+public class Player implements Celebrator
+	{
+	  public void celebrate1()
+	  {
+	      System.out.println("Tom Santos throws the ball up in the air when he scores to celebrate");
+	  }
+	  public void celebrate2()
+	  {
+	      System.out.println("Matt Rodgers celebrates by dancing the whip and nae nae");
+	  }
+	  
+	  public void celebrate3()
+	  {
+		  System.out.println("Aaron Stamos celebrates by singing the song '2K Magic' by Bruno Mars");
+		 
+	  }
+	  public void celebrate4()
+	  {
+		  System.out.println("Ben Jauregui celebrates by doing a backflip in the field");
+		  		
+	  }
+	  public void celebrate5()
+	  {
+		  System.out.println("Russell Brown celebrates by running to his teammates and jump around.");
+	  }
+	  public void main(String arg[])
+	  {
+	      Celebrator obj = new Player();
+	      obj. celebrate1();
+	     
+	      
+	  }
